8bb6d53038593d8f8678fc04ead52239f67dea56
Ankur Singh <135413489+Your-Random-Variable@users.noreply.github.com>
2023-06-10 10:10:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function showSection(sectionId) {
+  // Hide all sections
   const sections = document.querySelectorAll('section');
-  sections.forEach(section => {
+  sections.forEach((section) => {
     section.style.display = 'none';
   });
-
+  
+  // Show the selected section
   const selectedSection = document.getElementById(sectionId);
-  if (selectedSection) {
-    selectedSection.style.display = 'block';
-  }
+  selectedSection.style.display = 'block';
 }
+
+// Load the default section on page load
+window.addEventListener('load', () => {
+  showSection('home');
+});
