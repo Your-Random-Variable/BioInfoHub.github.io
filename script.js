17f2c1b3b02829a20b06cf65bf190cae2eddec39
Ankur Singh <135413489+Your-Random-Variable@users.noreply.github.com>
2023-06-19 14:21:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,89 +1,65 @@
 function showSection(sectionId) {
-  const sections = document.getElementsByTagName('section');
-  for (let i = 0; i < sections.length; i++) {
-    sections[i].style.display = 'none';
+  // Hide all sections
+  const sections = document.querySelectorAll('section');
+  sections.forEach(section => {
+    section.style.display = 'none';
+  });
+
+  // Show the selected section
+  const selectedSection = document.getElementById(sectionId);
+  selectedSection.style.display = 'block';
+
+  // Update the active link in the navigation
+  const navLinks = document.querySelectorAll('nav ul li a');
+  navLinks.forEach(link => {
+    link.classList.remove('active');
+  });
+
+  const selectedNavLink = document.querySelector(`nav ul li a[onclick="showSection('${sectionId}')"]`);
+  selectedNavLink.classList.add('active');
+
+  // Fetch and display news if the section is the "News" section
+  if (sectionId === 'news') {
+    fetchNews();
   }
-  document.getElementById(sectionId).style.display = 'block';
 }
 
-function searchNews() {
-  // RSS feed URL for bioinformatics news
-  const rssFeedUrl = 'https://journals.plos.org/ploscompbiol/feed/rss';
+function fetchNews() {
+  const newsContainer = document.getElementById('news-container');
+  newsContainer.innerHTML = 'Loading news...';
 
-  // Number of news items to display
-  const newsItemCount = 10;
+  const parser = new RSSParser();
+  const feedUrl = 'https://news.un.org/feed/subscribe/en/news/topic/health/feed/rss.xml'; // Replace with the URL of your RSS feed
 
-  // Fetch the RSS feed
-  fetch(rssFeedUrl)
-    .then(response => response.text())
-    .then(xmlString => {
-      // Parse the XML string to an XML document
-      const parser = new DOMParser();
-      const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
+  parser.parseURL(feedUrl)
+    .then(feed => {
+      let html = '';
 
-      // Retrieve the news items from the XML document
-      const items = xmlDoc.getElementsByTagName('item');
+      feed.items.forEach(item => {
+        const title = item.title;
+        const link = item.link;
+        const pubDate = new Date(item.pubDate).toLocaleDateString();
+        const description = item.contentSnippet;
+        const image = item.enclosure && item.enclosure.url;
 
-      // Update the newsContent element with the retrieved data
-      const newsContent = document.getElementById('newsContent');
-      // Clear previous content
-      newsContent.innerHTML = '';
-
-      // Iterate through the news items and create HTML elements for each item
-      for (let i = 0; i < Math.min(newsItemCount, items.length); i++) {
-        const item = items[i];
-        const title = item.querySelector('title').textContent;
-        const link = item.querySelector('link').textContent;
-        const description = item.querySelector('description').textContent;
-
-        const newsItemElement = document.createElement('div');
-        newsItemElement.classList.add('news-item');
-        newsItemElement.innerHTML = `
-          <h2>${title}</h2>
-          <p>${description}</p>
-          <a href="${link}" target="_blank">Read more</a>
+        html += `
+          <div class="news-item">
+            <div class="news-image">
+              <img src="${image}" alt="${title}">
+            </div>
+            <div class="news-content">
+              <h3 class="news-title"><a href="${link}">${title}</a></h3>
+              <p class="news-description">${description}</p>
+              <p class="news-pubdate">${pubDate}</p>
+            </div>
+          </div>
         `;
+      });
 
-        // Append the news item element to the newsContent element
-        newsContent.appendChild(newsItemElement);
-      }
+      newsContainer.innerHTML = html;
     })
     .catch(error => {
-      console.log('Error:', error);
+      console.error('Error fetching news:', error);
+      newsContainer.innerHTML = 'Failed to fetch news.';
     });
-}
-
-
-function searchProjects() {
-  const searchInput = document.getElementById('projectsSection').querySelector('input');
-  const searchTerm = searchInput.value.trim();
-  if (searchTerm !== '') {
-    // Perform search operation for projects using the searchTerm
-    // Example: fetch project data from an API
-    fetch(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=bioproject&term=${searchTerm}&retmax=10`)
-      .then(response => response.json())
-      .then(projectsData => {
-        // Update the projectsContent element with the retrieved data
-        const projectsContent = document.getElementById('projectsContent');
-        // Clear previous content
-        projectsContent.innerHTML = '';
-
-        // Iterate through the projectsData and create HTML elements for each project
-        projectsData.forEach(project => {
-          const projectElement = document.createElement('div');
-          projectElement.classList.add('project-item');
-          projectElement.innerHTML = `
-            <h2>${project.name}</h2>
-            <p>${project.description}</p>
-            <a href="${project.url}">View project</a>
-          `;
-
-          // Append the project element to the projectsContent element
-          projectsContent.appendChild(projectElement);
-        });
-      })
-      .catch(error => {
-        console.log('Error:', error);
-      });
-  }
 }
